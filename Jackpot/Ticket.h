79c2fc0fd24e93f7ba84a5680279df94a0e6771c
wiseguy16 <gergusa04@gmail.com>
2016-07-26 13:27:03 -0400

    Storyboard mostly laid out

---
+++ Jackpot/Ticket.h
@@ -0,0 +1,13 @@
+//
+//  Ticket.h
+//  Jackpot
+//
+//  Created by Gregory Weiss on 7/26/16.
+//  Copyright © 2016 Gregory Weiss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Ticket : NSObject
+
+@end
---
+++ Jackpot/Ticket.m
@@ -0,0 +1,28 @@
+//
+//  Ticket.m
+//  Jackpot
+//
+//  Created by Gregory Weiss on 7/26/16.
+//  Copyright © 2016 Gregory Weiss. All rights reserved.
+//
+
+#import "Ticket.h"
+
+@implementation Ticket
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self)
+    {
+  //      _operand1String = [[NSMutableString alloc] init];
+  //      _operand2String = [[NSMutableString alloc] init];
+  //      _operatorType = OperatorTypeNone;
+    }
+    
+    return self;
+}
+
+
+
+@end
